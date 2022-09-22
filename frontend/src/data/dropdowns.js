78f27b81e2655c3d84d18b5496494c0ef3ac5a2d
Ben Kirwan <bbk@Bens-MacBook-Air.local>
2022-09-22 13:38:30 +0100

    add build files

---
+++ frontend/src/data/dropdowns.js
@@ -0,0 +1,33 @@
+export const countryOptions = [
+  { key: 'uk', value: 'UK', flag: 'uk', text: 'UK' },
+  { key: 'eu', value: 'EU', flag: 'eu', text: 'Europe' },
+  { key: 'us', value: 'US', flag: 'us', text: 'US' },
+]
+export const tradeOptions = [
+    { key: 'tr', value: 'trade',  text: 'Trade' },
+    { key: 're', value: 'retail',  text: 'Retail' }
+]
+export const furnitureOptions = [
+    { key: 'di', value: 'dining',  text: 'Dining Table' },
+    { key: 'co', value: 'coffee',  text: 'Coffee Table' }
+]
+export const modelOptions = [
+    { key: 'an', value: 'Angel',  text: 'Angel' },
+    { key: 'ar', value: 'Arial',  text: 'Arial' },
+    { key: 'at', value: 'Atlantic',  text: 'Atlantic' },
+    { key: 'av', value: 'AVA',  text: 'AVA' },
+]
+export const shapeOptions = [
+    { key: 'ro', value: 'Round',  text: 'Round' },
+    { key: 're', value: 'Rectangular',  text: 'Rectangular' },
+    { key: 'ov', value: 'Oval',  text: 'Oval' },
+    { key: 'lo', value: 'Lozenge-shaped',  text: 'Lozenge-shaped' },
+    { key: 'ovsi', value: 'Oval single pedestal base', text: 'Oval single pedestal base' },
+]
+
+export const sizeOptions = [
+    { key: 'sm', value: 'S',  text: 'Small' },
+    { key: 'me', value: 'M',  text: 'Medium' },
+    { key: 'la', value: 'L',  text: 'Large' },
+    { key: 'xl', value: 'XL',  text: 'Extra Large' },
+]
